a6b5da1c8a49801db66f7dc3c8740f19ce300f39
Janarthanan Ravikumar <85826682+Jana0509@users.noreply.github.com>
2024-12-18 21:30:38 +0800

    Create install-wordPress.sh

---
+++ install-wordPress.sh
@@ -0,0 +1,20 @@
+# set permissions
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+sudo find /var/www -type f -exec sudo chmod 0664 {} \;
+chown apache:apache -R /var/www/html 
+
+# download wordpress files
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+sudo cp -r wordpress/* /var/www/html/
+
+# create the wp-config.php file
+sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+# edit the wp-config.php file
+sudo vi /var/www/html/wp-config.php
+
+# restart the webserver
+sudo service httpd restart
